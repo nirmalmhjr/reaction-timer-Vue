6ec078aa327f0887507767a24981057d939dcc97
Nirmal Maharjan <nirmalmhjr@gmail.com>
2023-06-08 16:59:41 +0545

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @endGame="endGame"/>
+  <Result v-if="showResult" :result="result"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Result },
+  data(){
+    return{ 
+      isPlaying: false,
+      delay: null,
+      result: null,
+      showResult: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() *4000
+      this.isPlaying = true
+      console.log(this.delay);
+    },
+    endGame(reactionTime){
+      this.result = reactionTime
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -20,7 +40,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color:white;
+  border: none ;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>Reaction time : {{ result }} ms</p>
+    <h2>{{ rankResponse }}</h2>
+
+</template>
+
+<script>
+export default {
+    props : ['result'],
+    data(){
+        return{
+            rankResponse: null
+        }
+    },
+    mounted(){
+            if(this.result < 250){
+            this.rankResponse='Ninja Finger'
+            } else if(this.result < 500){
+            this.rankResponse = 'you have fast fingers'
+            } else {
+            this.rankResponse = 'Hmm... slow pace'
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
